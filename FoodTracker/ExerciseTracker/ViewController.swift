921c3316426ea59b7542600167ce251b8e75645f
Asim Joshi <asimjoshi@Asims-MacBook-Pro.local>
2017-07-26 23:34:18 -0700

    Initial Commit

--- FoodTracker/ExerciseTracker/ViewController.swift
+++ FoodTracker/ExerciseTracker/ViewController.swift
@@ -1,6 +1,6 @@
 //
 //  ViewController.swift
-//  FirstApplication
+//  FoodTracker
 //
 //  Created by Asim Joshi on 6/22/17.
 //  Copyright © 2017 Asim Joshi. All rights reserved.
@@ -13,10 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print("Hello World");
-        HelloWorld();
     }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
